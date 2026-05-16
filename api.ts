6db7016df206df11865f1f43fe0a8b56b92badda
rprtr258 <poletansky@gmail.com>
2026-05-16 07:30:57 +0300

    Phase 10: GRPC Execution

--- api.ts
+++ api.ts
@@ -5,6 +5,7 @@ import {JQEmptyRequest, sendJQ} from "./database/jq.ts";
 import {DefaultMarkdown, sendMD} from "./database/md.ts";
 import {sendSQL} from "./database/sql.ts";
 import {RedisEmptyRequest, sendRedis} from "./database/redis.ts";
+import {sendGRPC, grpcMethods, grpcQueryFake, grpcQueryValidate} from "./database/grpc.ts";
 import {parseSpec, generateExampleRequest, fetchSpec} from "./database/http_source.ts";
 import {listTablesSQLSource, describeTableSQLSource, countRowsSQLSource, testSQLSource} from "./database/sql_source.ts";
 
@@ -176,6 +177,9 @@ export async function Perform(id: RequestID): Promise<PerformResponse> {
     break;
   case t.Kind.REDIS:
     result = await sendRedis(req.Data);
+    break;
+  case t.Kind.GRPC:
+    result = await sendGRPC(req.Data);
     break;
   default:
     throw new Error(`Perform not yet implemented for kind ${req.Kind}`);
